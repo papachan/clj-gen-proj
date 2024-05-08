1584b4d1a2bd497c05bf592632ec985bb7728aa4
Dan Loaiza <dan@resonancias.org>
2024-05-08 09:06:07 -0500

    handler changes

--- template/integrant-backend-app/src/com/example/handler.clj
+++ template/integrant-backend-app/src/com/example/handler.clj
@@ -14,54 +14,59 @@
    [reitit.interceptor.sieppari :as sieppari]
    [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
 
+(defn response-ok
+  [_req]
+  {:status 200
+   :body   {:message :ok}})
+
+(defn successful
+  [_req]
+  {:status 200
+   :body   {:success true}})
+
 (def app
   (ring/ring-handler
-    (ring/router
-      [["/api"
-        ["/v1"
-         ["/users"
-          ["/login"
-           {:get  {:responses {200 {:body [:map [:message :string]]}}
-                   :handler   (fn [_req]
-                                {:status 200
-                                 :body   {:message :ok}})}
-            :post {:summary   "login"
-                   :responses {200 {:body [:map [:message :boolean]]}}
-                   :handler   (fn [_req]
-                                {:status 200
-                                 :body   {:success true}})}}]]]]]
+   (ring/router
+    [["/api"
+      ["/v1"
+       ["/users"
+        ["/login"
+         {:get  {:responses {200 {:body [:map [:message :string]]}}
+                 :handler   #'response-ok}
+          :post {:responses {200 {:body [:map [:success :boolean]]}}
+                 :handler   #'successful}}]]]]]
 
-      {:exception pretty/exception
-       :data      {:coercion     (reitit.coercion.malli/create
-                                   {;; set of keys to include in error messages
-                                    :error-keys       #{:type :coercion :in :schema :value :errors :humanized}
-                                    ;; support lite syntax
-                                    ;; :lite true
-                                    ;; schema identity function (default: close all map schemas)
-                                    :compile          mu/closed-schema
-                                    ;; strip-extra-keys (effects only predefined transformers)
-                                    :strip-extra-keys true
-                                    ;; add/set default values
-                                    :default-values   true
-                                    ;; malli options
-                                    :options          nil})
-                   :muuntaja     m/instance
-                   :interceptors [(parameters/parameters-interceptor)
-                                  (muuntaja/format-negotiate-interceptor)
-                                  (muuntaja/format-response-interceptor)
-                                  (exception/exception-interceptor)
-                                  (muuntaja/format-request-interceptor)
-                                  (coercion/coerce-exceptions-interceptor)
-                                  (coercion/coerce-response-interceptor)
-                                  (coercion/coerce-request-interceptor)]
-                   :middleware [[wrap-json-body {:keywords? true}]
-                                wrap-json-response
-                                ;; cors
-                                ;; middleware/allow-cross-origin
-                                ]}})
-    (ring/routes
-      (ring/create-default-handler
-        {:not-found (constantly {:status  404
-                                 :headers {"Content-Type" "application/json"}
-                                 :body    (json/write-str {:error true})})})
-      {:executor sieppari/executor})))
+    {:exception pretty/exception
+     :data      {:coercion     (reitit.coercion.malli/create
+                                { ;; set of keys to include in error messages
+                                 :error-keys       #{:type :coercion :in :schema :value :errors :humanized}
+                                 ;; support lite syntax
+                                 ;; :lite true
+                                 ;; schema identity function (default: close all map schemas)
+                                 :compile          mu/closed-schema
+                                 ;; strip-extra-keys (effects only predefined transformers)
+                                 :strip-extra-keys true
+                                 ;; add/set default values
+                                 :default-values   true
+                                 ;; malli options
+                                 :options          nil})
+                 :muuntaja     m/instance
+                 :interceptors [(parameters/parameters-interceptor)
+                                (muuntaja/format-negotiate-interceptor)
+                                (muuntaja/format-response-interceptor)
+                                (exception/exception-interceptor)
+                                (muuntaja/format-request-interceptor)
+                                (coercion/coerce-exceptions-interceptor)
+                                (coercion/coerce-response-interceptor)
+                                (coercion/coerce-request-interceptor)]
+                 :middleware [[wrap-json-body {:keywords? true}]
+                              wrap-json-response
+                              ;; cors
+                              ;; middleware/allow-cross-origin
+                              ]}})
+   (ring/routes
+    (ring/create-default-handler
+     {:not-found (constantly {:status  404
+                              :headers {"Content-Type" "application/json"}
+                              :body    (json/write-str {:error true})})})
+    {:executor sieppari/executor})))
