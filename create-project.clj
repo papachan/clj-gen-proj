43927815400465c48f3a1ba06f6f0dbaf4ddc8f0
Dan Loaiza <dan@resonancias.org>
2024-06-12 16:50:25 -0500

    cleaning previous output files

--- create-project.clj
+++ create-project.clj
@@ -34,27 +34,28 @@
          "- eg: bb create-project.clj [template-type] [your-namespace]"))
   (when (not (contains? templates (str/lower-case template-name)))
     (die "Please use a valid template, here are the valid templates:" (str/join " or " (map #(str "`" % "`") templates))))
-  (let [project-name "generated"
-        template-dir (str "template/" (str/lower-case template-name) "/")
-        dir          (io/file project-name)]
-    (io/make-parents (io/file dir "src"))
-    (if (contains? #{"fullstack"} template-name)
-      (doseq [path ["src/backend/" "src/frontend/"]]
-        (fs/create-dirs (io/file dir (str path (ns->path project-ns)))))
-      (fs/create-dirs (io/file dir (str "src/" (ns->path project-ns)))))
-    (doseq [src (->> (io/file template-dir)
-                     (file-seq)
-                     (filter #(.isFile %)))
-            :let [relative (-> (.getPath src)
-                               (str/replace #"\\" "/")
-                               (str/replace-first (re-pattern (str ".*?" template-dir)) "")
-                               (str/replace "com/example" (ns->path project-ns)))
-                  dest (io/file dir relative)
-                  path (.getPath (.getParentFile dest))]]
-      (fs/create-dirs path)
-      (spit dest
-            (-> src
-                slurp
-                (str/replace "com.example" project-ns))))))
+  (let [dir (io/file "generated")]
+    (when (fs/directory? dir)
+      (fs/delete-tree dir))
+    (let [template-dir (str "template/" (str/lower-case template-name) "/")]
+      (io/make-parents (io/file dir "src"))
+      (if (contains? #{"fullstack"} template-name)
+        (doseq [path ["src/backend/" "src/frontend/"]]
+          (fs/create-dirs (io/file dir (str path (ns->path project-ns)))))
+        (fs/create-dirs (io/file dir (str "src/" (ns->path project-ns)))))
+      (doseq [src (->> (io/file template-dir)
+                       (file-seq)
+                       (filter #(.isFile %)))
+              :let [relative (-> (.getPath src)
+                                 (str/replace #"\\" "/")
+                                 (str/replace-first (re-pattern (str ".*?" template-dir)) "")
+                                 (str/replace "com/example" (ns->path project-ns)))
+                    dest (io/file dir relative)
+                    path (.getPath (.getParentFile dest))]]
+        (fs/create-dirs path)
+        (spit dest
+              (-> src
+                  slurp
+                  (str/replace "com.example" project-ns)))))))
 
 (apply -main *command-line-args*)
