a6096d37ed18d8e704859980d0ce4821687f51a4
Dan Loaiza <dan@resonancias.org>
2024-06-18 15:40:21 -0500

    new layout

--- template/fullstack/src/frontend/com/example/views.cljs
+++ template/fullstack/src/frontend/com/example/views.cljs
@@ -1,39 +1,59 @@
 (ns com.example.views
   (:require
    [re-frame.core :as re-frame]
-   [com.example.events :as events]
-   [com.example.myapp :as webapp]
-   [com.example.subs]))
+   [basic.example.events :as events]
+   [basic.example.myapp :as webapp]
+   [basic.example.subs]))
 
 
 (defn navigation
   []
-  [:div
-   {:className "px-32 py-6 mt-4 text-center bg-white"}
+  [:nav
+   {:className "bg-gray-200 p-6"}
    [:div
-    [:button
-     {:on-click #(re-frame/dispatch [::events/push-state {:route ::webapp/home}])}
-     "home"]
-    [:button
-     {:on-click #(re-frame/dispatch [::events/push-state {:route ::webapp/subpage}])}
-     "subpage 1"]]])
+    {:className "container mx-auto"}
+    [:ul
+     {:className "flex justify-around"}
+     [:li
+      [:a
+       {:href     "#"
+        :on-click #(re-frame/dispatch [::events/push-state {:route ::webapp/home}])
+        :className "hover:text-gray-300"}
+       "home"]]
+     [:li
+      [:a
+       {:href     "#"
+        :on-click #(re-frame/dispatch [::events/push-state {:route ::webapp/subpage}])
+        :className "hover:text-gray-300"}
+       "subpage 1"]]]]])
+
+(defn container
+  [name]
+  [:main
+   {:className "container mx-auto mt-10"}
+   [:div
+    {:className "flex justify-center items-center h-screen"}
+    [:div
+     {:className "bg-white p-10 rounded-lg shadow-lg"}
+     [:h1
+      {:className "text-2xl font-bold mb-4"}
+      name]
+     [:p
+      {:className "text-gray-700"}
+      "Loremp ipsum"]]]])
 
 (defn homepage
   []
   [:div
-   {:className "flex items-center justify-center min-h-screen bg-gray-100"}
-   (navigation)
-   [:div
-    {:className "px-32 py-6 mt-4 text-left bg-white"}
-    [:div
-     "home"]]])
+   {:className "bg-gray-100"}
+   [:header
+    (navigation)]
+   (container "home")])
 
 (defn subpage
   []
   [:div
-   {:className "flex items-center justify-center min-h-screen bg-gray-100"}
-   (navigation)
-   [:div
-    {:className "px-32 py-6 mt-4 text-left bg-white"}
-    [:div
-     "Subpage 1"]]])
+   {:className "bg-gray-100"}
+   [:header
+    (navigation)]
+   (container "subpage 1")])
