d285b6179d0a15ec939b5699cbf13abdf6a25645
dan loaiza <dan@resonancias.org>
2025-04-07 14:57:13 -0500

    fix integrant template

--- template/integrant-app/env/dev/user.clj
+++ template/integrant-app/env/dev/user.clj
@@ -1,23 +1,24 @@
 (ns user
   "Tools for interactive development with the REPL. This file should
   not be included in a production build of the application."
-  (:require [clojure.string :as str]
-            [clojure.java.io :as io]
-            [clojure.java.javadoc :refer [javadoc]]
-            [clojure.pprint :refer [pprint]]
-            [clojure.repl :refer [apropos dir doc find-doc pst source]]
-            [clojure.test :as test]
-            [clojure.tools.namespace.repl :as r]
-            [integrant.core :as ig]
-            [integrant.repl :as ig-repl]
-            [com.something.system :as system-util]))
+  (:require
+   [clojure.string :as str]
+   [clojure.java.io :as io]
+   [clojure.java.javadoc :refer [javadoc]]
+   [clojure.pprint :refer [pprint]]
+   [clojure.repl :refer [apropos dir doc find-doc pst source]]
+   [clojure.test :as test]
+   [clojure.tools.namespace.repl :as r]
+   [integrant.core :as ig]
+   [integrant.repl :as ig-repl]
+   [com.example.system :as sys]))
 
 
 (r/set-refresh-dirs "src" "env/dev")
 
 (defn- integrant-prep!
   []
-  (ig-repl/set-prep! #(ig/prep system-util/config))
+  (ig-repl/set-prep! #(ig/prep sys/config))
   (ig-repl/prep))
 
 (defn reset
@@ -32,6 +33,7 @@
 
 (defn start
   []
+  (println "starting server at port" (get-in sys/config [:ring/jetty :port]))
   (integrant-prep!)
   (ig-repl/init)
   (ig-repl/go))
