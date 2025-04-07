a2a640e07e0797e0b88f6da0234a4a857806969f
Dan <dan@resonancias.org>
2025-04-07 11:06:55 -0500

    add uix new template (#14)
    * react19 template with uix
    
    * indent
    
    * add bun run in parallel task
    
    * tiny change

---
+++ template/shadow-uix-app/src/com/example/main.cljs
@@ -0,0 +1,16 @@
+(ns com.example.main
+  (:require
+   [uix.dom]
+   [uix.core :as uix :refer [defui $]]))
+
+(defui app []
+  ($ :.flex.flex-col.justify-center.items-center.h-screen
+     ($ :h1.text-4xl.mb-4.font-bold
+        "Hello Clojurescript")
+     ($ :h2.text-2xl.font-semibold.text-blue-500
+        "Something here")))
+
+(defonce root (uix.dom/create-root (js/document.getElementById "root")))
+
+(defn ^:export init []
+  (uix.dom/render-root ($ app) root))
