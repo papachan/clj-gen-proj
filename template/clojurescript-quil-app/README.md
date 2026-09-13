b3cd63a5375bd5ae1443b9233efd0f13d970b167
Dan <38855+papachan@users.noreply.github.com>
2026-09-13 07:15:58 -0500

    new quil project template (#15)
    * new template
    
    * fix encoding html templates
    
    * update cdn url
    
    * update gitignore
    
    * smooth
    
    * update README.md
    
    * fix issue with start methods
    
    * update README
    
    * change title

---
+++ template/clojurescript-quil-app/README.md
@@ -0,0 +1,1 @@
+## web project with clojurescript and quil sketch
---
+++ template/clojurescript-quil-app/src/com/example/app.cljs
@@ -0,0 +1,54 @@
+(ns com.example.app
+  (:require
+   [quil.core :as q :include-macros true]
+   [quil.middleware :as m]))
+
+(defn setup []
+  (q/frame-rate 60)
+  (q/color-mode :hsb)
+  (q/smooth)
+  {:color  0
+   :angle  0
+   :paused false})
+
+(defn update-state [state]
+  (if (:paused state)
+    state
+    (assoc state
+           :color (mod (+ (:color state) 0.35) 255)
+           :angle (+ (:angle state) 0.05))))
+
+(defn mouse-pressed [state {:keys [x y]}]
+  (if (and (<= 0 x (q/width))
+           (<= 0 y (q/height)))
+    (update state :paused not)
+    state))
+
+(defn draw-state [state]
+  (q/background 240)
+  (q/fill (:color state) 255 255)
+  (let [angle (:angle state)
+        x     (* 150 (q/cos angle))
+        y     (* 150 (q/sin angle))]
+    (q/with-translation [(/ (q/width) 2)
+                         (/ (q/height) 2)]
+      (q/ellipse x y 100 100))))
+
+(q/defsketch sketch-app
+  :host "app"
+  :size [500 500]
+  :setup setup
+  :update update-state
+  :draw draw-state
+  :mouse-pressed mouse-pressed
+  :middleware [m/fun-mode])
+
+(defn ^:export re-render []
+  ;; after shadow-cljs hot-reloads code.
+  ;; This function is called implicitly by its annotation.
+  (.log js/console "reload")
+  sketch-app)
+
+(defn ^:export init []
+  (.log js/console "init")
+  sketch-app)
